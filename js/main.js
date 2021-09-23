14a39b50d181aebdded13ff3d76c6b69f7f8a35a
jennyvaittinen <73814101+jennyvaittinen@users.noreply.github.com>
2021-09-23 13:46:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
-var purple = [43, 74, 49];
+var purple = [0, 0, 100];
 var letterColors = [purple]; //red, orange, green, blue, 
 
 drawName(wordToDisplay, letterColors);
